5f2a7ef8ad107de449e17e29f54b7b7b32d41a6e
boydclifton <bstinnett65@gmail.com>
2025-03-11 15:50:05 -0400

    Finished prep 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int favNumberSquared = FavNumberSquared(favNumber);
+        DisplayResult(name, favNumberSquared);
+
+
+
+        
+        
+        
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int FavNumberSquared(int favNumber)
+        {
+            int favNumberSquared = favNumber * favNumber;
+            return favNumberSquared;
+        }
+
+        static void DisplayResult(string name, int favNumberSquared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {favNumberSquared}.");
+        }
+
+
+    //    static void DisplayMessage()
+    //    {
+    //     Console.WriteLine("Hello World!");
+    //    }
+
+    //    static void DisplayPersonalMessage(string userName)
+    //    {
+    //     Console.WriteLine($"Hello {userName}");
+    //    }
+
+    //    static int AddNumbers(int first, int second)
+    //    {
+    //     int sum = first + second; 
+    //     return sum;
+    //    }
     }
 }
