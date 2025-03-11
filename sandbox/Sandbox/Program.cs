5d6c25d16c2123665583375fe958577501b69447
boydclifton <bstinnett65@gmail.com>
2025-03-11 12:41:58 -0400

    revert Sandbox to original

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,33 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // if (x > y)
-        // {
-        //     Console.WriteLine("greater");
-        // }
-
-        // string school = "BYU-Idaho";
-        // Console.WriteLine($"I am studying at {school}");
         Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("This is in C#");
-
-        // int number = 5;
-        // number = 8;
-        // number = number + 3;
-
-        // string color = "blue";
-
-        // if (number > 3)
-        // {
-        //     Console.WriteLine("");
-        //     Console.WriteLine("");
-        // }
-        // Console.Write("What is your favorite color? ");
-        // string color = Console.ReadLine();
-        // Console.WriteLine($"Your color is {color}");
-        
-
-        
     }
 }
 
