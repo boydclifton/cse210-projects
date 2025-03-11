5e5e705007b79e51439f7eaa210ce802fe2c4ab9
boydclifton <bstinnett65@gmail.com>
2025-03-11 14:32:03 -0400

    Finished prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+
+        do 
+        {Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guessNumber = -1; 
+
+        while (magicNumber != guessNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+            
+            if (guessNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+            else if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+
+        }}
+
+        Console.WriteLine("You guessed the magic number!");
+        Console.WriteLine("Would you like to play again?");
+        response = Console.ReadLine();
+        } while (response == "yes");
+
+        Console.WriteLine("Thanks for playing!");
+
+
+
+        
+
     }
 }
