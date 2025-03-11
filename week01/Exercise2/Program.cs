bc9a6cb4fe0a5005d6067a43c11d6d37dc49afc5
boydclifton <bstinnett65@gmail.com>
2025-03-11 13:41:02 -0400

    Finished prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = percent % 10;
+        string symbol = "";
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            symbol = "+";
+        }
+        else if (lastDigit < 3 && letter != "A" && letter != "F")
+        {
+            symbol = "-";
+        }
+
+
+        Console.WriteLine($"You have earned the grade of {letter}{symbol}");
+
     }
 }
