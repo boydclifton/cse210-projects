8f7325755e2bf67062a4ea1c7c52419ee1bf1f3c
boydclifton <bstinnett65@gmail.com>
2025-09-03 12:31:37 -0400

    Reset the progress for exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine();
-        Console.Write($"Your name is {last_name}, {first_name} {last_name}");
+        
     }
 }
