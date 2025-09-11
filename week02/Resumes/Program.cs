1750c1a828e734ea0694a21fc419478d854c1c8f
boydclifton <bstinnett65@gmail.com>
2025-09-11 13:10:18 -0400

    Job and Resume completion

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Facebook";
+        job1._jobTitle = "Information Technologist";
+        job1._startYear = 2011;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job2._company = "Adobe";
+        job2._jobTitle = "Design Manager";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Boyd Stinnet";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+
+
     }
+
 }
